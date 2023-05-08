229c9a54afa5c5094ea29135b94375996d303e2c
Kasieńka <Kasieka@DESKTOP-J1SM6ND>
2023-05-08 20:20:42 +0200

    Dodaj pliki projektów.

---
+++ Models/StudentContex.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+}
